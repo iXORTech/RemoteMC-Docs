c25d7f5d9b35abded7ac346898dc221fc90a0a68
Cubik65536 <cubik65536@cubik65536.top>
2022-06-26 23:07:44 -0400

    Add Markdown parser for Sphinx

--- source/conf.py
+++ source/conf.py
@@ -38,7 +38,10 @@ extensions = [
     'sphinx.ext.viewcode',
     'sphinx_copybutton',
     'sphinx_sitemap',
+    'myst_parser',
 ]
+
+source_suffix = ['.rst', '.md']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
