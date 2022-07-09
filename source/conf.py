b79974e648c1e798f2f24260b08d3d26a755d7b4
Cubik65536 <cubik65536@cubik65536.top>
2022-07-08 22:30:53 -0400

    style(conf.py): Place setup at the right place

--- source/conf.py
+++ source/conf.py
@@ -1,13 +1,3 @@
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -20,9 +10,9 @@ sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
 
 
 # -- Project information -----------------------------------------------------
